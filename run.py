d52a9c50b5ff790baeae75687076e2e78fc84b24
MaryAnne45 <mary.jereh@gmail.com>
2018-04-02 22:03:34 +0300

    three functions added in the run.py two used to create an account and one used to save the user info

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3.6
+from user import User
+from credentials import Info
+
+def create_account(f_name,m_name,e_mail):
+    new_user = User(f_name,m_name,e_mail)
+    return new_user
+def create_credentials(face_bookp,e_mailp):
+    new_cred = Info(face_bookp,e_mailp)
+    return new_cred
+def save_account(user):
+    user.save_user()
+    
