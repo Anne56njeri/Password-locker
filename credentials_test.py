12c115111c1fec51c7a20695e9eac00e3dfcb6e5
MaryAnne45 <mary.jereh@gmail.com>
2018-04-02 21:12:09 +0300

    a test case added to test whether the info is saved

--- credentials_test.py
+++ credentials_test.py
@@ -7,5 +7,9 @@ class TestInfo(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.new_info.face_bookp,"mary.anne")
         self.assertEqual(self.new_info.email_p,"anne.mary")
+    def test_save_info(self):
+        self.new_info.save_info()
+        self.assertEqual(len(Info.info_list),1)
+
 if __name__ == '__main__':
     unittest.main()
